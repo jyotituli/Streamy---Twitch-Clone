63f0e72bc03521ba6235ffef16a6d052ec810f2d
jyotituli <jyotituli27@gmail.com>
2020-10-04 00:38:16 +0530

    Authentication

--- Streamy/client/src/components/Header.js
+++ Streamy/client/src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+//cid 3584645026-1f5121s33i1b458tdtrbhmufo923mud7.apps.googleusercontent.com
 
 const Header = () => {
     return (
@@ -11,6 +14,7 @@ const Header = () => {
                 <Link to="/" className="item">
                     All Streams
             </Link>
+            <GoogleAuth/>
             </div>
         </div>
     )
